0c422bdf0ab7f9f2468149f77a79e0bf20ca7fb6
Alex Chandra Hanif <alexchandra1998@gmail.com>
2024-02-21 17:51:32 +0700

    chore: update detail surah

--- src/pages/DetailSurah.jsx
+++ src/pages/DetailSurah.jsx
@@ -1,16 +1,12 @@
 import axios from "axios";
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { MdBrightnessLow } from "react-icons/md";
 
 const DetailSurah = () => {
   const navigate = useNavigate();
   const { id } = useParams();
-  let [quran, setQuran] = useState([]);
-
-  let [surah, setSurah] = useState([]);
+  const [quran, setQuran] = useState([]);
+  const [surah, setSurah] = useState([]);
 
   useEffect(() => {
     axios({
@@ -44,32 +40,38 @@ const DetailSurah = () => {
 
   return (
     <div className="w-full min-h-screen">
-      <div>
-        <div className="flex overflow-scroll gap-3">
-          {quran?.map((el) => {
-            return (
-              <p
-                key={el.nomor}
-                className="w-[90px]  whitespace-nowrap"
-                onClick={() => handleSurah(el.nomor)}
-              >
-                {el.namaLatin}
-              </p>
-            );
-          })}
+      <div className="w-full">
+        {/* Quick Access */}
+        <div className="w-full h-14 bg-white flex items-center p-2 overflow-scroll gap-3 sticky top-0 z-10">
+          {quran?.map((el) => (
+            <p
+              key={el.nomor}
+              className="w-[90px] whitespace-nowrap cursor-pointer"
+              onClick={() => handleSurah(el.nomor)}
+            >
+              {el.namaLatin}
+            </p>
+          ))}
         </div>
 
-        <div>
+        {/* Detail */}
+        <div className="bg-slate-200  p-4">
+          {" "}
+          {/* Tambahkan margin-top */}
           <p>{surah?.data?.nama}</p>
           <p>{surah?.data?.namaLatin}</p>
           <p>{surah?.data?.jumlahAyat}</p>
           <p>{surah?.data?.tempatTurun}</p>
           <p>{surah?.data?.arti}</p>
         </div>
-        <div>
+
+        {/* Ayat */}
+        <div className="mt-4 p-4">
+          {" "}
+          {/* Tambahkan margin-top */}
           {surah?.data?.ayat.map((el) => (
             <div key={el.nomorAyat}>
-              <p className="">{el.nomorAyat}</p>
+              <p>{el.nomorAyat}</p>
               <p>{el.teksArab}</p>
               <p>{el.teksLatin}</p>
               <p>{el.teksIndonesia}</p>
